96e1a4bed071fb05405cb41d2035bbafaa64bf6e
Shajib96 <shajibahamedornob@gmail.com>
2023-12-09 20:40:20 +0600

    Array and object Destructuring

---
+++ RecapJS/destructuring.js
@@ -0,0 +1,12 @@
+const color= ["red" , "yellow" , "blue", "green" , "orange"]
+const [color1 , color2 , color3] =color
+console.log(color1 , color2 , color3);
+
+
+const student ={
+    name:"Shajib",
+    age: 21,
+    skill:["Js" , "HTML" ,"CSS" , "Tailwind"]
+};
+const [fist , second] = student.skill;
+console.log(fist , second)
