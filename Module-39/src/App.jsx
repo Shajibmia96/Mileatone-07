0c97b17bc9184c90bfc7c958bd88b3b3c07a6169
Shajib96 <shajibahamedornob@gmail.com>
2023-12-14 00:49:25 +0600

    explore evant handles on react

--- Module-39/src/App.jsx
+++ Module-39/src/App.jsx
@@ -4,11 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+  
+   function handlesClick(){
+      alert("button click")
+   };
+
+   const handlesClick2 =() =>{
+       alert("button click2")
+   };
+
+   const addFive =(num) =>{
+      alert (num +10)
+   }
 
   return (
     <>
-        <h1>Now we explore react cor concept</h1>
+        <h3>Now we explore react cor concept</h3>
+        <button onClick={handlesClick}>Click me</button>
+        {/* Array function */}
+        <button onClick={handlesClick2}>Click me2</button>
+        {/* Anonymous function */}
+        <button onClick={() =>{alert("handlesClick03")}}>Click me3</button>
+        <button onClick={() =>addFive(10)}>Five</button>
+
+      
     </>
   )
 }
