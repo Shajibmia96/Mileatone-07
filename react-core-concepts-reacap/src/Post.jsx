2746b9657325d1a9b9ac5663302a5cdeb2999cd6
Shajib96 <shajibahamedornob@gmail.com>
2023-12-16 13:13:17 +0600

    Exlore  how to data load and show UI

--- react-core-concepts-reacap/src/Post.jsx
+++ react-core-concepts-reacap/src/Post.jsx
@@ -1,10 +1,11 @@
 import './post.css'
 export default function Post ({post}){
      console.log(post)
-     const {id , body} = post
+     const {id , body ,userId} = post
     return(
         <div className="box">
             <h4> id : {id} </h4>
+            <p>userId :{userId}</p>
             <p>body : {body} </p>
         </div>
     )
