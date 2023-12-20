34cf9cc855d5d811210cbddd9c1fdada323fd226
Shajib96 <shajibahamedornob@gmail.com>
2023-12-20 11:22:10 +0600

    Conditional CSS and conditional style

--- Module-40/src/Components/Countries/Countries.jsx
+++ Module-40/src/Components/Countries/Countries.jsx
@@ -13,13 +13,32 @@ const Countries = () => {
              fetch("https://restcountries.com/v3.1/all")
              .then ( res => res.json())
              .then( data => setCountries(data))
-        } , [])
+        } , []);
+
+       const [visitedCountry , setVisitedCountry] = useState([])
+
+       const handlesVisited =country =>{
+        console.log("We find out visited country")
+            console.log(country)
+       }
+
     return (
+
+          
         <div>
               <h4>Countries : {countries.length}</h4>
+
+                <div>
+                    <h3>Visited countries</h3>
+                    <ul>
+
+                    </ul>
+                </div>
               <div className="countries-container">
                  {
-                countries.map( country =><Country key={country.cca3} 
+                countries.map( country =><Country 
+                    key={country.cca3} 
+                    handlesVisited={handlesVisited}
                     country={country}></Country>)
                 }
               </div>
--- Module-40/src/Components/Country/Country.jsx
+++ Module-40/src/Components/Country/Country.jsx
@@ -1,22 +1,33 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
-    console.log(country)
+const Country = ({country , handlesVisited}) => {
+
+    // console.log(country)
+    
+
       const {name , flags , area , capital, population , region} = country;
-      console.log(country);
+    //   console.log(country);
+
        const [visited , setVisited] = useState(false)
+
          const handlesClick =() =>{
              setVisited(!visited)
-         }
+         };
+
+         console.log(handlesVisited)
+
+
+
     return (
-        <div className="country">
-              <p>Name : {name?.common}</p>
-              <p>Alt : {flags?.alt}</p>
+        <div className= {`country  ${visited ? "visited" : "None"}`}>
+              <p style={{color : visited ? "purple" : "orange", fontSize: visited ? "40px" : "20px"}}>Name : {name?.common}</p>
+              {/* <p>Alt : {flags?.alt}</p> */}
               <img src={flags?.png} alt="" />
-              <p>Area :{area}</p>
+              <p>Area :{area}</p> 
               <p>Capital :{capital}</p>
               <p> Population :{population}</p>
               <p>Region :{region}</p>
+              <button> Mark country visited</button> <br /><br />
               <button onClick={handlesClick}>{visited ? "Visited" : "I wanna going a day"}</button><br />
               {visited ? "This country was visited by me" : "This country did'n visited"}
         </div>
