40c10069133c41502d5ebceff186d02692579f6c
Shajib96 <shajibahamedornob@gmail.com>
2024-01-16 08:02:22 +0600

    added my-cinema-Project file

---
+++ my-Cinema-Project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Cart from "./Component/Cart/Cart";
+import Home from "./Component/Home/Home";
+const App = () => {
+  return (
+    <>
+          <Home></Home> 
+          <Cart></Cart> 
+    </>
+  );
+};
+
+export default App;
---
+++ my-Cinema-Project/src/Component/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>I dream become a developer</h1>
+        </div>
+    );
+};
+
+export default Cart;
