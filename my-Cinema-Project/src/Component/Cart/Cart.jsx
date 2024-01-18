0fe944fbe44ac6037fe4fecc047f711237f2a6b7
Shajib96 <shajibahamedornob@gmail.com>
2024-01-18 08:15:20 +0600

    creact a dynamic layout

--- my-Cinema-Project/src/Component/Cart/Cart.jsx
+++ my-Cinema-Project/src/Component/Cart/Cart.jsx
@@ -1,22 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Cart.css'
-const Cart = ({actor}) => {
-    console.log(actor)
+const Cart = () => {
+    // console.log("this is actor",actor)
     return (
         <div>
-            <div className="card">
-                   <div >
-                    <img className="photo" src="https://i.ibb.co/QnXmyzf/karen-gillan-1200.jpg" alt="" />
-                   </div>
-                   <h3>Name : Atonny disosa</h3>
-                   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, tenetur. Et, optio!</p>
-                   <div className="info">
-                      <p>Salary : 200</p>
-                      <p>Writer</p>
-                   </div>
-                   <button className='cart-btn'>Selected</button>
-                </div>
+           
         </div>
     );
 };
--- my-Cinema-Project/src/Component/Home/Home.jsx
+++ my-Cinema-Project/src/Component/Home/Home.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
 import './Home.css'
-import Cart from '../Cart/Cart';
+
 
 
 const Home = () => {
     // UseState
      const [Actors , setActors] =useState([])
+    //  console.log(Actors)
     // useEffect and fetch data 
        useEffect(()=>{
             fetch('/public/data.json')
@@ -14,14 +15,27 @@ const Home = () => {
             .then(data => setActors(data))
        } ,[])
        
-    //    console.log(Actors)
+       console.log(Actors)
     return (
         <div> 
              <div className="Home-container">
                 <div className="card-container">
                  {
-                    Actors.map(actor =><Cart key={actor.id} actor={actor}></Cart>)
-                 }
+                    // eslint-disable-next-line react/jsx-key
+                    Actors.map(actor =><div key={actor.id} className="card">
+                        
+                    <div >
+                     <img className="photo" src={actor.image} alt="" />
+                    </div>
+                    <h3>Name :{actor.name}</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, tenetur. Et, optio!</p>
+                    <div className="info">
+                       <p>Salary : {actor.salary}</p>
+                       <p>{actor.role}</p>
+                    </div>
+                    <button className='cart-btn'>Selected</button>
+                 </div>)   
+                 } 
                 </div>
                 <div className="cart-container">
                     <h3>This is cart sections</h3>
