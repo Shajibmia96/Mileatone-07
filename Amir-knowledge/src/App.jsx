190a4db3a66257bd627a0b93e000e88c76c32f55
Shajib96 <shajibahamedornob@gmail.com>
2024-02-01 19:59:19 +0600

    adde tailwild css

---
+++ Amir-knowledge/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-fuchsia-300'>Amir-knowledge-cafe</h1>
+        
+    </>
+  )
+}
+
+export default App
